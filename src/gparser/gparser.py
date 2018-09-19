c94de77c68ee25b2120a88163b47b2b283e0316c
Gaufoo <zhongzc_arch@outlook.com>
2018-09-19 16:46:57 +0800

    monad bind

--- src/gparser/gparser.py
+++ src/gparser/gparser.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-
 __author__ = 'Gaufoo, zhongzc_arch@outlook.com'
+
+__see__ = 'http://www.cs.nott.ac.uk/~pszgmh/monparsing.pdf'
 
 from collections import namedtuple
 from typing import Callable
@@ -38,14 +39,12 @@ class LocatedText:
 """
 Success = namedtuple('Success', ['value'])
 
-
 """
 # ParseError
 
 解释失败后State.result包含的错误信息
 """
 ParseError = namedtuple('ParseError', ['msg'])
-
 
 """
 # State(result: T, text: LocatedText)
@@ -55,12 +54,48 @@ ParseError = namedtuple('ParseError', ['msg'])
 State = namedtuple('State', ['result', 'text'])
 
 
-"""
-# Parser(fn: LocatedText -> State)
+class Parser:
+    """
+    Parser(fn: LocatedText -> State)
 
-解析器，接受一个LocatedText，返回解析后的状态
-"""
-Parser = namedtuple('Parser', ['fn'])
+    解析器，运行时接受一个LocatedText，返回解析后的状态
+    """
+    fn = ...  # type: Callable[[LocatedText], State]
+
+    def __init__(self, fn: Callable[[LocatedText], State]):
+        self.fn = fn
+
+    def run(self, inp: str) -> State:
+        return self.fn(LocatedText(inp))
+
+    def __call__(self, *args, **kwargs):
+        return self.run(*args, **kwargs)
+
+    def flatmap(self, func):
+        """
+        Parser[A] -> (A -> Parser[B]) -> Parser[B]
+
+        @:see : https://en.wikipedia.org/wiki/Monad_(functional_programming)
+        """
+
+        def inner(loc: LocatedText) -> State:
+            state = self.fn(loc)
+            if isinstance(state.result, Success):
+                return func(state.result.value).fn(state.text)
+            else:
+                return state
+
+        return Parser(inner)
+
+    def map(self, func):
+        return self.flatmap(lambda v: just(func(v)))
+
+    def then(self, parser):
+        return self.flatmap(lambda _: parser)
+
+    def __rshift__(self, func):
+        return self.then(func)
+
 
 
 def run_parser(parser: Parser, inp: str) -> State:
@@ -69,9 +104,9 @@ def run_parser(parser: Parser, inp: str) -> State:
 
     :param parser: 构建好的解析器
     :param inp: 待解析字符串
-    :return: 解析完的状态，返回Success或者ParseError
+    :return State: 解析完的状态，返回Success或者ParseError
     """
-    return parser.fn(LocatedText(inp))
+    return parser.run(inp)
 
 
 def satisfy(pred: Callable[[str], bool]) -> Parser:
@@ -79,8 +114,9 @@ def satisfy(pred: Callable[[str], bool]) -> Parser:
     若下一个字符满足判断条件(pred)，则解析成功
 
     :param pred: 下一个字符该满足的条件
-    :return: 相应的Parser
+    :return Parser: 相应的Parser
     """
+
     def inner(loc: LocatedText) -> State:
         if loc.isEOF():
             return State(ParseError("再无输入可解析"), loc)
@@ -101,14 +137,46 @@ def label(parser: Parser, msg: str) -> Parser:
 
     :param parser: 需要修饰的Parser
     :param msg: 进一步的错误信息
-    :return: 修饰过的Parser
+    :return Parser: 修饰过的Parser
     """
+
     def inner(loc: LocatedText) -> State:
         state = parser.fn(loc)
         if isinstance(state.result, Success):
             return state
         else:
             return State(ParseError(msg), state.text)
+
+    return Parser(inner)
+
+
+# def label1(msg: str):
+#     def decorator(func):
+#         def wrapper(*args, **kwargs):
+#             def inner(loc: LocatedText) -> State:
+#                 state = func(*args, **kwargs).fn(loc)
+#                 if isinstance(state.result, Success):
+#                     return state
+#                 else:
+#                     return State(ParseError(msg), state.text)
+#
+#             return Parser(inner)
+#
+#         return wrapper
+#
+#     return decorator
+
+
+def just(v) -> Parser:
+    """
+    直接让v作为解析成功的值返回，且不消耗任何字符
+    :param v: 返回的成功值
+    :return Parser: 未改变的Parser状态，但解析成功的值变为v
+    """
+
+    def inner(loc: LocatedText) -> State:
+        return State(Success(v), loc)
+
     return Parser(inner)
 
 
@@ -117,7 +185,7 @@ def char(x: str) -> Parser:
     解析某一个字符
 
     :param x: 需要解析的字符
-    :return: 解析此字符的Parser
+    :return Parser: 解析此字符的Parser
     """
     return label(satisfy(lambda c: c == x), 'Excepted: {}'.format(x))
 
@@ -126,10 +194,46 @@ def space() -> Parser:
     """
     解析空格、\t、\n、\r等space值
 
-    :return: 解析space值的Parser
+    :return Parser: 解析space值的Parser
     """
     return label(satisfy(str.isspace), 'Excepted: space')
 
 
+def digit() -> Parser:
+    """
+    解析数字 [0-9]
+
+    :return Parser: 解析数字的Parser
+    """
+    return label(satisfy(str.isdigit), 'Excepted: digit')
+
+
+def alpha() -> Parser:
+    """
+    解析字母 [A-Za-z]
+
+    :return Parser: 解析字母的Parser
+    """
+    return label(satisfy(str.isalpha), 'Excepted: alpha')
+
+
+def one_of(chrs: str) -> Parser:
+    """
+    解析所给字符串里的其中一个字符
+    :param chrs: 所述的待解析字符串
+    :return Parser: 解析所给字符其中之一的Parser
+    """
+    return label(satisfy(lambda c: c in chrs), 'Excepted: one of ' + ','.join(chrs))
+
+
+def none_of(chrs: str) -> Parser:
+    """
+    解析所给字符串以外的任意一个字符
+    :param chrs: 所述的待排除字符串
+    :return Parser: 解析所给字符以外字符的Parser
+    """
+    return label(satisfy(lambda c: c not in chrs), 'Excepted: none of ' + ', '.join(chrs))
+
+
 if __name__ == '__main__':
-    print(run_parser(char('a'), 'abc'))
+    print(run_parser(one_of('{}[]'), '${a}'))
--- test/gparsertest/gparserSuite.py
+++ test/gparsertest/gparserSuite.py
@@ -20,11 +20,36 @@ def test_context(t, inp: (Parser, str), result, remaining: str):
 
 
 class ParserTest(unittest.TestCase):
+    def test_map(self):
+        test_context(self, (digit().map(eval), '123'), Success(1), '23')
+        test_context(self, (alpha().map(lambda x: x.upper()), 'abc'), Success('A'), 'bc')
+
+    def test_bind(self):
+        test_context(self, (digit().flatmap(lambda c1: digit()
+                                            .flatmap(lambda c2: digit()
+                                                     .flatmap(lambda c3:
+                                                              just(int(c1 + c2 + c3))))), '1234'), Success(123), '4')
+        test_type(self, (digit().flatmap(lambda c1: digit()
+                                         .flatmap(lambda c2: digit()
+                                                  .flatmap(lambda c3:
+                                                           just(int(c1 + c2 + c3))))), '12a4'), ParseError, 'a4')
+
+
+    def test_then(self):
+        test_context(self, (digit().then(digit()), '1234'), Success('2'), '34')
+        test_type(self, (digit().then(alpha()), '1234'), ParseError, '234')
+        test_context(self, (digit() >> digit(), '1234'), Success('2'), '34')
+        test_type(self, (digit() >> alpha(), '1234'), ParseError, '234')
+
     def test_satisfy(self):
         test_context(self, (satisfy(lambda c: c == 'a'), 'abc'), Success('a'), 'bc')
         test_type(self, (satisfy(lambda c: c == 'b'), 'abc'), ParseError, 'abc')
         test_context(self, (satisfy(str.isdigit), '123'), Success('1'), '23')
         test_type(self, (satisfy(str.isalnum), ''), ParseError, '')
+
+    def test_just(self):
+        test_context(self, (just('&'), '987'), Success('&'), '987')
+        test_context(self, (just(999), 'cn'), Success(999), 'cn')
 
     def test_char(self):
         test_context(self, (char('['), '[abc]'), Success('['), 'abc]')
@@ -41,6 +66,24 @@ class ParserTest(unittest.TestCase):
         test_context(self, (space(), '\n 7'), Success('\n'), ' 7')
         test_context(self, (space(), '\r 7'), Success('\r'), ' 7')
         test_type(self, (space(), '\a 7'), ParseError, '\a 7')
+
+    def test_digit(self):
+        test_context(self, (digit(), '123'), Success('1'), '23')
+        test_context(self, (digit(), '0?'), Success('0'), '?')
+        test_type(self, (space(), 'abc'), ParseError, 'abc')
+
+    def test_alpha(self):
+        test_context(self, (alpha(), 'ZXY'), Success('Z'), 'XY')
+        test_context(self, (alpha(), 'abc'), Success('a'), 'bc')
+        test_type(self, (alpha(), '123'), ParseError, '123')
+
+    def test_one_of(self):
+        test_context(self, (one_of('{}[]'), '[]'), Success('['), ']')
+        test_type(self, (one_of('{}[]'), '${a}'), ParseError, '${a}')
+
+    def test_none_of(self):
+        test_context(self, (none_of('{}[]'), '${a}'), Success('$'), '{a}')
+        test_type(self, (none_of('{}[]'), '[]'), ParseError, '[]')
 
     if __name__ == '__main__':
         unittest.main()
